d87644701396c39b52d5ffae777a9cce4a90227f
Arnold Retrepo <restrepo_arnold@outlook.com>
2017-02-21 23:08:57 -0500

    Navegación Móvil, Envío de Newsletter

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-$(document).foundation()
+$(document).foundation();
+
+
+// Navegacion
+$('[data-toggle-dia]').click(function(){
+	const panel = $(this).data('toggleDia');
+	console.log(panel);
+	$('#lineup-tabs').foundation('selectTab', panel);
+});
+
+// Offcanavas
+
+const canvas = $('#offCanvas');
+const sticky = $('#stcky');
+canvas.find('li').click(function(ev){
+	canvas.foundation('close');
+	setTimeout(function() {
+		sticky.css('left', '0px');
+	}, 0);
+});
+
